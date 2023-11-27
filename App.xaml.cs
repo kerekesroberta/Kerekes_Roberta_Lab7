a6ac9f1e33c144cab3038810687700a41e0b3bf1
Roberta <kerekesroberta@yahoo.com>
2023-11-27 20:47:16 +0200

    lab8 done

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,28 @@
-﻿namespace Kerekes_Roberta_Lab7
+﻿using System;
+using Kerekes_Roberta_Lab7.Data;
+using System.IO;
+using Kerekes_Roberta_Lab7.Data;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Kerekes_Roberta_Lab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+    
         public App()
         {
             InitializeComponent();
